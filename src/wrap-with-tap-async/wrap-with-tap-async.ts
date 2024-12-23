7770a29c3634136ca6bc1b72d3e3fb6787c8d0c5
Daniel Kreiger <dankreiger@gmail.com>
2024-12-23 15:29:04 +0100

    fix: try bun config

--- src/wrap-with-tap-async/wrap-with-tap-async.ts
+++ src/wrap-with-tap-async/wrap-with-tap-async.ts
@@ -1,11 +1,6 @@
-import { pipeAsync } from '../pipe-async/pipe-async.utils.ts';
 import { tapAsync } from '../tap-async/index.ts';
 import type { AsyncFunction } from '../types.ts';
 
 export const wrapWithTapAsync = <const T>(...fns: AsyncFunction<T>[]) => {
   return fns.map((fn) => tapAsync(fn));
-};
-
-export const pipeTappedAsync = <const T>(...fns: AsyncFunction<T>[]) => {
-  return pipeAsync(...wrapWithTapAsync(...fns));
 };
