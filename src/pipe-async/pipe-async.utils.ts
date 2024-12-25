29de46cb4be94ee83d13ed8f7da58a7e7a912364
Daniel Kreiger <dankreiger@gmail.com>
2024-12-25 01:38:28 +0100

    fix: export aliases for fns

--- src/pipe-async/pipe-async.utils.ts
+++ src/pipe-async/pipe-async.utils.ts
@@ -1,9 +1,14 @@
 type AsyncFunction<T> = (input: T) => Promise<T>;
 
 export const pipeAsync = <T>(...fns: AsyncFunction<T>[]) => {
-	return (input: T): Promise<T> =>
-		fns.reduce<Promise<T>>(
-			(promise, fn) => promise.then(fn),
-			Promise.resolve(input),
-		);
+  return (input: T): Promise<T> =>
+    fns.reduce<Promise<T>>(
+      (promise, fn) => promise.then(fn),
+      Promise.resolve(input)
+    );
 };
+
+/**
+ * @alias pipeAsync
+ */
+export const pipe = pipeAsync;
--- src/tap-async/tap-async.utils.ts
+++ src/tap-async/tap-async.utils.ts
@@ -1,6 +1,11 @@
 export const tapAsync = <const T>(fn: (input: T) => Promise<unknown>) => {
-	return async (input: T): Promise<T> => {
-		await fn(input);
-		return input;
-	};
+  return async (inp: T): Promise<T> => {
+    await fn(inp);
+    return inp;
+  };
 };
+
+/**
+ * @alias for tapAsync
+ */
+export const tap = tapAsync;
